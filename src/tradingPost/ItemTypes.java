a9e77fdaf23893f644c8881deface851213729b9
Mike <mdpswaan@avans.nl>
2018-12-17 20:53:09 +0100

    ItemTypes

--- src/tradingPost/ItemTypes.java
+++ src/tradingPost/ItemTypes.java
@@ -1,8 +1,7 @@
 package tradingPost;
 
 public enum ItemTypes {
-	WEAPONS, FOOD, POTIONS, DRINKS, ARMOUR, APPEARAL, MEDICAL
-	
+	APPEARAL, ARMOUR, DRINKS, FOOD, MATERIALS, POTIONS, SHIELDS, WEAPONS_MAGIC, WEAPONS_MELEE, WEAPONS_RANGED	
 	//WEAPONS: items used directly in combat
 	//FOOD: consumeable substances
 	//POTIONS: drinkables with special effect
